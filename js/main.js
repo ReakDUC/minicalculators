05e1c4a2d1d3c6730786b19b86d4eebad874a014
ReakDUC <reaklearning.duc2025@gmail.com>
2025-02-21 14:50:30 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,76 @@
+//create function miniCalculator
+// function miniCalculator (operators) {
+//     let num1 = parseFloat (document.getElementById('num1').value);
+//     let num2 = parseFloat (document.getElementById('num2').value);
+//     let output = document.getElementById('output1')
+
+//     if (isNaN(num1) || isNaN(num2)){
+//         output.value = 'Ivalid value'
+//         return ;
+//     }
+
+//     let result ;
+//     switch (operators){
+//         case '+':
+//             result = num1 + num2;
+//             break;
+//         case '-': 
+//             result = num1 - num2;
+//             break;
+//         case '*':
+//             result = num1 * num2;
+//             break;
+//         case '/':
+//             if (num2 === 0){
+//                 output.value = 'Cannot divide y zero';
+//                 return ;
+//             }
+//             result = num1 / num2;
+//             break;
+//         default:
+//             output.value = 'Error';
+//             return ;
+//     }
+//     output.value = result;
+// }
+
+// function miniCalculator (operators){
+//     let num1 = parseFloat (document.getElementById('num1').value);
+//     let num2 = parseFloat (document.getElementById('num2').value);
+//     let output = document.getElementById('output1')
+    
+//     if (isNaN(num1) || isNaN(num2)){
+//         output.value = 'I Love U <3'
+//         return ;
+//     }
+    
+//     let result ;
+//     switch (operators){
+//         case '+':
+//             result = num1 + num2;
+//             break;
+//         case '-': 
+//             result = num1 - num2;
+//             break;
+//         case '*':
+//             result = num1 * num2;
+//             break;
+//         case '/':
+//             if (num2 === 0){
+//                 output.value = 'Cannot divide by zero';
+//                 return ;
+//             }
+//             result = num1 / num2;
+//             break;
+//         default:
+//             output.value = 'Error';
+//             return ;
+//     }
+//     output.value = result;
+// }
+
+
+function miniCalculator(operators) {
+    let output = document.getElementById('output1');
+    output.value = 'Jork men ng';
+}
